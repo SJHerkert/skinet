fe24a00b46c66c14a4dbda6bdfe6924ba9e8492b
Simon Herkert <simon.herkert@outlook.com>
2025-04-15 18:22:33 +0300

    Part 10, added assets, navbar, font-awesome, httpclient

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavBarComponent } from "./nav-bar/nav-bar.component";
+import { HttpClient } from '@angular/common/http';
 
 //Decorator
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavBarComponent],
+  providers: [HttpClient],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'SkiNet';
+
+  //Inject http client
+  constructor(private http: HttpClient){}
+
+  ngOnInit(): void {
+    this.http.get('https/localhost:5079/api/products').subscribe((response: any) => {
+      console.log(response);
+    }, error => {
+      console.log(error);
+    });  
+  }
 }
+
